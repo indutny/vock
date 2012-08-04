9fd05225abff5f0bcea004cf70168e226089deac
Fedor Indutny <fedor.indutny@gmail.com>
2012-08-04 20:15:16 +0300

    examples: fix

--- example/delay.js
+++ example/delay.js
@@ -5,7 +5,19 @@ var rate = 8000,
 
 a.ondata = function(data) {
   setTimeout(function() {
-    a.enqueue(data);
+    a.enqueue(2, data);
+  }, 4000);
+
+  setTimeout(function() {
+    a.enqueue(0, data);
+  }, 3000);
+
+  setTimeout(function() {
+    a.enqueue(1, data);
   }, 2000);
+
+  setTimeout(function() {
+    a.enqueue(3, data);
+  }, 1000);
 };
 a.start();
--- example/echo.js
+++ example/echo.js
@@ -4,6 +4,6 @@ var rate = 24000,
     a = new binding.Audio(rate, rate / 100);
 
 a.ondata = function(data) {
-  a.enqueue(data);
+  a.enqueue(0, data);
 };
 a.start();

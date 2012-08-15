cd35a1c802720fe8bfd7ecf0701ce0242b00b4b1
Fedor Indutny <fedor.indutny@gmail.com>
2012-08-15 23:49:26 +0700

    gyp: unbreak build on linux

---
+++ deps/speex/speex/include/speex/speex_config_types.h
@@ -0,0 +1,13 @@
+#ifndef __SPEEX_TYPES_H__
+#define __SPEEX_TYPES_H__
+
+#include <stdint.h>
+
+/* these are filled in by configure */
+typedef int16_t spx_int16_t;
+typedef uint16_t spx_uint16_t;
+typedef int16_t spx_int32_t;
+typedef unsigned uint16_t spx_uint32_t;
+
+#endif
+
